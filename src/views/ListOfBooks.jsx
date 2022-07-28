5e72b2bae70f488e4ce3d465f526466effb98305
MargauxDF <margaux.defilippis@gmail.com>
2022-07-28 10:51:03 +0200

    List of books with image and title

---
+++ src/views/ListOfBooks.jsx
@@ -0,0 +1,50 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import "../styles/ListOfBooks.css";
+
+const ListOfBooks = () => {
+  const [books, setBooks] = useState(null);
+  const getListOfBooks = async () => {
+    let booksList;
+    await axios.post(
+      'https://api.lelivrescolaire.fr/graphql',
+      {
+        'query': 'query{viewer{books{hits{id displayTitle url subjects{name}levels{name}valid}}}}',
+        'variables': {}
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8'
+        }
+      }
+    ).then((res) => {
+      booksList = res.data;
+    }).catch((err) => {
+      console.log(err);
+    });
+
+    return booksList;
+  }
+
+  useEffect(() => {
+    getListOfBooks().then((booksList) => {
+      setBooks(booksList.data.viewer.books.hits);
+      console.log(booksList.data.viewer.books.hits);
+    });
+  },[]);
+
+  return (
+   <div className="books-list">
+      {books && books.map((book) => {
+        return (
+          <div className="books-container" key={book.id}>
+            <h3>{book.displayTitle}</h3>
+            <img className="books-img" src={book.url} alt={book.displayTitle} />
+          </div>
+        )
+      })}
+   </div>
+ );
+}
+
+export default ListOfBooks;
