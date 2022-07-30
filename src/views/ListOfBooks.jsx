6f8f36a8f78451e38974227bb42b4e52e615f81e
MargauxDF <margaux.defilippis@gmail.com>
2022-07-30 13:15:02 +0200

    add subjects filtering on list of books

--- src/views/ListOfBooks.jsx
+++ src/views/ListOfBooks.jsx
@@ -2,8 +2,10 @@ import React, {useEffect, useState} from "react";
 import axios from "axios";
 import "../styles/ListOfBooks.css";
 import { Link } from "react-router-dom";
+import SubjectFilter from "../components/SubjectFilter";
 
 const ListOfBooks = () => {
+  const [activeSubject, setActiveSubject] = useState("");
   const [books, setBooks] = useState(null);
   const getListOfBooks = async () => {
     let booksList;
@@ -33,18 +35,36 @@ const ListOfBooks = () => {
     });
   },[]);
 
+  // Filter books by subject
+  const subjects = books?.flatMap((book) => book.subjects?.map((subject) => subject.name));
+
+  const onlyUnique = (value, index, self) => {
+    return self.indexOf(value) === index;
+  }
+
+  // Get unique subjects to display in the select option
+  const subjectsList = subjects?.filter(onlyUnique);
+
   return (
    <div className="books-list">
+     <SubjectFilter
+        subjectsList={subjectsList}
+        activeSubject={activeSubject}
+        setActiveSubject={setActiveSubject}
+      />
       {books && books.map((book) => {
-        return (
-          <div className="books-container" key={book.id}>
-            <Link to={book.valid && `/book/${book.id}`} state={{ book }}>
-              <h3 className={book.valid ? "" : "not-valid-books-title"}>{book.displayTitle}</h3>
-              <img className={book.valid ? "valid-books-img" : "not-valid-books-img"} src={book.url} alt={book.displayTitle} />
-            </Link>
-          </div>
-        )
-      })}
+        if (activeSubject === "" || book.subjects.some((subject) => subject.name === activeSubject)) {
+          return (
+            <div className="books-container" key={book.id}>
+              <Link to={book.valid && `/book/${book.id}`} state={{ book }}>
+                <h3 className={book.valid ? "" : "not-valid-books-title"}>{book.displayTitle}</h3>
+                <img className={book.valid ? "valid-books-img" : "not-valid-books-img"} src={book.url} alt={book.displayTitle} />
+              </Link>
+            </div>
+          )
+      } else {
+        return null;
+      }})}
    </div>
  );
 }
